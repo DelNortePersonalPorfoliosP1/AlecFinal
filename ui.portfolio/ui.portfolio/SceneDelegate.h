1e1bb4f8bcc730eff428cc7f3bcbc3b6e9c4224a
Alec Linyard <alec.linyard@gmail.com>
2019-11-04 08:58:48 -0800

    initial

---
+++ ui.portfolio/ui.portfolio/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ui.portfolio
+//
+//  Created by Alec Linyard on 11/4/19.
+//  Copyright © 2019 Alec Linyard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
