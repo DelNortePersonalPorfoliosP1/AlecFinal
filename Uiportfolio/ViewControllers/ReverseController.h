72a26bb8fdbd5a5e5bacbd2e863371829764cac4
Alec Linyard <alec.linyard@gmail.com>
2019-12-10 09:23:56 -0800

    added labels

---
+++ Uiportfolio/ViewControllers/ReverseController.h
@@ -0,0 +1,18 @@
+//
+//  ReverseController.h
+//  Uiportfolio
+//
+//  Created by Alec Linyard on 12/4/19.
+//  Copyright © 2019 JM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReverseController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *inputText;
+@property (strong, nonatomic) IBOutlet UILabel *output1Text;
+@property (strong, nonatomic) IBOutlet UILabel *output2Text;
+@property (strong, nonatomic) IBOutlet UILabel *output3Text;
+
+@end
