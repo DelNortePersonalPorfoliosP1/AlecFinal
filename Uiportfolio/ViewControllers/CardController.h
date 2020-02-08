6a957ec5da2cedc727b7cdb204db2d871d65cb89
Alec Linyard <alec.linyard@gmail.com>
2020-02-07 23:06:44 -0800

    Created storyboard, card images, .m and .h for war card came.

---
+++ Uiportfolio/ViewControllers/CardController.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Uiportfolio
+//
+//  Created by Alec Linyard on 2/7/20.
+//  Copyright © 2020 JM. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface CardController : UIViewController
+
+
+@end
---
+++ Uiportfolio/ViewControllers/CardController.m
@@ -0,0 +1,15 @@
+//
+//  CardController.m
+//  Uiportfolio
+//
+//  Created by Alec Linyard on 2/7/20.
+//  Copyright © 2020 JM. All rights reserved.
+//
+
+#include "control.h"
+#import "CardController.h"
+
+@implementation CardController
+
+
+@end
